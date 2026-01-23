5d282f6c8f58f477eff01ace30e50a670415e226
Riyaz Tevar <riyaz_tk@yahoo.in>
2026-01-23 10:00:52 +0530

    Fix: correct module path in main.tf

--- terraform/tictactoe-dev/bootstrap/main.tf
+++ terraform/tictactoe-dev/bootstrap/main.tf
@@ -1,5 +1,5 @@
 module "tictactoe_backend_setup" {
-  source                 = "../modules/bootstrap"
+  source                 = "../../modules/bootstrap"
   region                 = "us-east-1"
   backend_s3_bucket      = "riyaz-tictactoe-backend-bucket-20260122"
   backend_dynamodb_table = "riyaz-backend-locker-dynamodb-table-20260122"
